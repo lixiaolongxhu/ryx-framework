b9b9f8bdec34308cb1fd41a7e965fbb84b0805f7
li.xiaolong <lxl@riyouxi.cn>
2015-10-15 11:07:22 +0800

    spring mvc 注解事务配置

--- src/main/java/com/sample/mvc/controller/TestController.java
+++ src/main/java/com/sample/mvc/controller/TestController.java
@@ -1,11 +1,8 @@
 package com.sample.mvc.controller;
 
 import java.util.Date;
-import java.util.Random;
 import java.util.UUID;
-
 import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,6 +30,16 @@ public class TestController {
 	public Integer exception(){
 		testService.exception();
 		return 1; 
+	}
+	
+	@RequestMapping(value="tran")
+	public void transaction(){
+		User user=new User();
+		user.setUuid(UUID.randomUUID().toString());
+		user.setLoginName("lxl"+ ToolHelper.getRandomNumber(4));
+		user.setPassword("password");
+		user.setCreateTime(DateUtil.dateToString(new Date(), DateUtil.DATAFORMAT0));
+		testService.tran(user);
 	}
 	
 	@RequestMapping(value="/insert")
--- src/main/java/com/sample/mvc/service/TestService.java
+++ src/main/java/com/sample/mvc/service/TestService.java
@@ -3,6 +3,9 @@ package com.sample.mvc.service;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.framework.core.dao.BasicDaoImp;
 import com.framework.core.exception.BizException;
@@ -24,6 +27,14 @@ public class TestService {
 	public void exception() {
 		// TODO Auto-generated method stub
 		throw new BizException("统一异常处理返回测试内容");
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
+	public void tran(User user) {
+		
+		basicdao.insert(user);
+		
+		throw new BizException("异常回滚测试");
 	}
 	
 	
