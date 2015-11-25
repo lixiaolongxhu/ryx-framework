767c107a4dff2575d62788642eda5d88eb84d9bd
lixiaolongxhu <lixiaolongxhu@163.com>
2015-11-25 17:22:27 +0800

    extjs group  example

--- src/main/webapp/web-extjs/sample/view/common/LeftMenu.js
+++ src/main/webapp/web-extjs/sample/view/common/LeftMenu.js
@@ -22,10 +22,29 @@ Ext.define('RYIVS.view.common.LeftMenu', {
 	
 	
 	
-	
+	 features: [{
+         id: 'group',
+         ftype: 'groupingsummary',
+         groupHeaderTpl: '{name}',
+         hideGroupedHeader: true,
+         enableGroupingMenu: false
+     }],
 	
 	// 定义 colums
-	columns : [ {
+	columns : [ 
+	            
+	    {
+        text: '类型',
+        flex: 1,
+        tdCls: 'type',
+        sortable: true,
+        dataIndex: 'type',
+        hideable: false,
+        summaryType: 'count',
+        summaryRenderer: function(value, summaryData, dataIndex) {
+            return ((value === 0 || value > 1) ? '(' + value + ' Tasks)' : '(1 Task)');
+        }
+    },{
 		width : 40,
 		dataIndex : 'type',
 		align : 'right',
@@ -43,73 +62,46 @@ Ext.define('RYIVS.view.common.LeftMenu', {
 			allowBlank : false
 		},
 
-	},
-	{
-        text: '类型',
-        flex: 1,
-        tdCls: 'type',
-        sortable: true,
-        dataIndex: 'type',
-        hideable: false,
-        summaryType: 'count',
-        summaryRenderer: function(value, summaryData, dataIndex) {
-            return ((value === 0 || value > 1) ? '(' + value + ' Tasks)' : '(1 Task)');
-        }
-    }
+	}
+	
 	],
 
-	features : [ Ext.create('Ext.grid.feature.Grouping', {
-		groupHeaderTpl : [ '{groupValue:this.formatValue}: 共 ({rows.length}) 个', {
-			formatValue : function(value) {
-				return ry.constant.trans(value, ry.constant.resourceType);
-			}
-		} ]
-	}) ],
 
-	viewConfig : {
-		plugins : {
-			ddGroup : 'ddgEquipment',
-			ptype : 'gridDragPlugin',
-			enableDrop : true,
-			enableDrag : true
-		}
-	},
-
-	// 是否使能拖放
-	enableDrag : function(en) {
-		var dragPlugin = this.getView().getPlugin();
-		if (en) {
-			dragPlugin.enable();
-		} else {
-			dragPlugin.disable();
-		}
-	},
-
-	// 是否允许删除
-	enableDelete : function(en) {
-		if (en) {
-			this.query('button[itemId="buttonDelete"]')[0].show();
-		} else {
-			this.query('button[itemId="buttonDelete"]')[0].hide();
-		}
-	},
-	// 得到Res的Store
-	getResStore : function() {
-		return this.store;
-	},
+//	// 是否使能拖放
+//	enableDrag : function(en) {
+//		var dragPlugin = this.getView().getPlugin();
+//		if (en) {
+//			dragPlugin.enable();
+//		} else {
+//			dragPlugin.disable();
+//		}
+//	},
+//
+//	// 是否允许删除
+//	enableDelete : function(en) {
+//		if (en) {
+//			this.query('button[itemId="buttonDelete"]')[0].show();
+//		} else {
+//			this.query('button[itemId="buttonDelete"]')[0].hide();
+//		}
+//	},
+//	// 得到Res的Store
+//	getResStore : function() {
+//		return this.store;
+//	},
 	initComponent : function() {
 		this.callParent(arguments);
-		this.store.group('type');
-		//this.query('button[itemId="buttonAdd"]')[0].hide();
-
-		// 禁止编辑
-		this.on('beforeedit', function(e) {
-			return false;
-		});
-
-		// 不使用拖放
-		this.enableDrag(false);
-		
+//		this.store.group('type');
+//		//this.query('button[itemId="buttonAdd"]')[0].hide();
+//
+//		// 禁止编辑
+//		this.on('beforeedit', function(e) {
+//			return false;
+//		});
+//
+//		// 不使用拖放
+//		this.enableDrag(false);
+//		
 		//controller.viewResGrid = this;
 
 	},
